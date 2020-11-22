f99d302aed4d938c87b50d9ae076eb31d2669a0a
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2020-11-22 17:30:08 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-while True:
-    tempF = input.temperature(TemperatureUnit.FAHRENHEIT)
-    tempC = input.temperature(TemperatureUnit.CELSIUS)
-    # print("Temperature (F):" + tempF + " - Temperature (C):" + tempC)
-    print("Current Room Temperature: " + tempF + "°F - " + tempC + "°C")
+# while True:
+#     tempF = input.temperature(TemperatureUnit.FAHRENHEIT)
+#     tempC = input.temperature(TemperatureUnit.CELSIUS)
+#     # print("Temperature (F):" + tempF + " - Temperature (C):" + tempC)
+#     print("Current Room Temperature: " + tempF + "°F - " + tempC + "°C")
+
+
+light.set_brightness(10)
+tempF = input.temperature(TemperatureUnit.FAHRENHEIT)
+
+# if tempF > 60:
+#     light.set_pixel_color(5, light.rgb(255, 0, 0))
+
+# if tempF > 70:
+#     light.set_pixel_color(5, light.rgb(255, 0, 0))
+# elif temp < 70 and tempF > 40:
+#     light.set_pixel_color(5, light.rgb(0, 255, 0))
+# else:
+#     light.set_pixel_color(5, light.rgb(0, 0, 255))
+
+if tempF > 70:
+    light.set_all(light.rgb(255,0,0))
+elif tempF < 70 and tempF > 40:
+    light.set_all(light.rgb(0,255,0))
+else:
+    light.set_all(light.rgb(0,0,255))
+    
--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-let tempF: number;
-let tempC: number;
-while (true) {
-    tempF = input.temperature(TemperatureUnit.Fahrenheit)
-    tempC = input.temperature(TemperatureUnit.Celsius)
-    //  print("Temperature (F):" + tempF + " - Temperature (C):" + tempC)
-    console.log("Current Room Temperature: " + tempF + "°F - " + tempC + "°C")
+//  while True:
+//      tempF = input.temperature(TemperatureUnit.FAHRENHEIT)
+//      tempC = input.temperature(TemperatureUnit.CELSIUS)
+//      # print("Temperature (F):" + tempF + " - Temperature (C):" + tempC)
+//      print("Current Room Temperature: " + tempF + "°F - " + tempC + "°C")
+light.setBrightness(10)
+let tempF = input.temperature(TemperatureUnit.Fahrenheit)
+//  if tempF > 60:
+//      light.set_pixel_color(5, light.rgb(255, 0, 0))
+//  if tempF > 70:
+//      light.set_pixel_color(5, light.rgb(255, 0, 0))
+//  elif temp < 70 and tempF > 40:
+//      light.set_pixel_color(5, light.rgb(0, 255, 0))
+//  else:
+//      light.set_pixel_color(5, light.rgb(0, 0, 255))
+if (tempF > 70) {
+    light.setAll(light.rgb(255, 0, 0))
+} else if (tempF < 70 && tempF > 40) {
+    light.setAll(light.rgb(0, 255, 0))
+} else {
+    light.setAll(light.rgb(0, 0, 255))
 }
+
